71dd4feb4c338c951447f1ad45eafded45179800
Chris Leonard <cleonard@godaddy.com>
2022-12-01 17:06:36 -0600

    solutions for 2022-12-01

---
+++ 2022/01/solution-01.py
@@ -0,0 +1,31 @@
+"""
+Solution to the first 1-Dec-2022 puzzle for Advent of Code (https://adventofcode.com)
+"""
+
+debug = 0
+
+with open("./2022/01/input.txt", mode="r") as f:
+    current_calory_total = 0
+    current_elf_number = 1
+    best_elf = {"elf_number": 1, "elf_calories": 0}
+    for line in f:
+        try:
+            current_calories = int(line.strip())
+            current_calory_total += current_calories
+        except ValueError as e:
+            if line.strip() == "":
+                if current_calory_total > best_elf["elf_calories"]:
+                    best_elf["elf_number"] = current_elf_number
+                    best_elf["elf_calories"] = current_calory_total
+
+                if debug:
+                    print(
+                        f"Elf {current_elf_number} is carrying {current_calory_total} calories."
+                    )
+
+                current_elf_number += 1
+                current_calory_total = 0
+            else:
+                raise ValueError from e
+
+print(best_elf)
---
+++ 2022/01/solution-02.py
@@ -0,0 +1,39 @@
+"""
+Solution to the second 1-Dec-2022 puzzle for Advent of Code (https://adventofcode.com)
+"""
+
+from sortedcontainers import SortedList
+
+debug = 0
+number_of_elves = 3
+
+with open("./2022/01/input.txt", mode="r") as f:
+    current_elf_number = 1
+    current_calory_total = 0
+    best_elves = SortedList()
+
+    for line in f:
+        try:
+            current_calories = int(line.strip())
+            current_calory_total += current_calories
+        except ValueError as e:
+            if line.strip() == "":
+
+                if len(best_elves) < number_of_elves:
+                    best_elves.add(current_calory_total)
+                elif current_calory_total > best_elves[0]:
+                    best_elves.pop(0)
+                    best_elves.add(current_calory_total)
+                    print(f"Adding elf {current_elf_number} with {current_calory_total} calories.")
+
+                if debug:
+                    print(
+                        f"Elf {current_elf_number} is carrying {current_calory_total} calories."
+                    )
+
+                current_elf_number += 1
+                current_calory_total = 0
+            else:
+                raise ValueError from e
+
+print(f"The three largest calorie loads are {best_elves} and add up to {sum(best_elves)} total calories.")
